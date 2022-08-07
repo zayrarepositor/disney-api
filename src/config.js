41bea37cd394f1b7203abdf2200415fd69fd4cfb
zayrarepositor <zayra.contacto@gmail.com>
2022-08-07 16:58:02 -0300

    env var

--- src/config.js
+++ src/config.js
@@ -5,7 +5,5 @@ export default {
   PORT: process.env.PORT,
   MAIL_USER: process.env.MAIL_USER,
   MAIL_PASS: process.env.MAIL_PASS,
-  DB_HOST: process.env.DB_HOST,
-  DB_USER: process.env.DB_USER,
-  DB_PASS: process.env.DB_PASS,
+  DATABASE_URL: process.env.DATABASE_URL,
 };
--- src/db/db.js
+++ src/db/db.js
@@ -1,14 +1,13 @@
 import { Sequelize } from "sequelize";
 import config from "../config.js";
 
-export const sequelize = new Sequelize(
-  "disney",
-  config.DB_USER,
-  config.DB_PASS,
-  {
-    host: config.DB_HOST,
-    dialect: "postgres",
-    logging: false,
-    native: false,
-  }
-);
+export const sequelize = new Sequelize(config.DATABASE_URL, {
+  logging: false,
+  native: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
